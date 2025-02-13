43f0cd83ef7b64b6df61865e1039abc14a036f48
gitdavehorsley <44680821+gitdavehorsley@users.noreply.github.com>
2025-02-13 10:17:05 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import AdminPanel from "./components/AdminPanel";
+// src/App.tsx
+import React from 'react';
+// If you want to render your AdminPanel, import it here:
+// import AdminPanel from './components/AdminPanel';
 
 function App() {
-    return (
-        <div className="App">
-            <AdminPanel />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello, JerseyGirl!</h1>
+      {/* Or <AdminPanel /> if using the AdminPanel component */}
+    </div>
+  );
 }
 
 export default App;
