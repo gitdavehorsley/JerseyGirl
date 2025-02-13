b7e962c8bc31d0513d6be999ba2f707af8866ba1
gitdavehorsley <44680821+gitdavehorsley@users.noreply.github.com>
2025-02-13 16:21:09 -0500

    Update table.tsx

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -1,40 +1,18 @@
-// src/components/ui/table.tsx
 import React from 'react';
+import { Table, TableHead, TableRow, TableCell } from '@material-ui/core';
 
-export const Table = ({ className = '', ...props }: React.TableHTMLAttributes<HTMLTableElement>) => (
-  <div className="w-full overflow-auto">
-    <table
-      className={`w-full caption-bottom text-sm ${className}`}
-      {...props}
-    />
-  </div>
-);
+const MyTable = () => {
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell headers="email">Email</TableCell>
+          <TableCell headers="phone">Phone</TableCell>
+        </TableRow>
+      </TableHead>
+      {/* Table body content */}
+    </Table>
+  );
+};
 
-export const TableHeader = ({ className = '', ...props }: React.HTMLAttributes<HTMLTableSectionElement>) => (
-  <thead className={`bg-gray-100 ${className}`} {...props} />
-);
-
-export const TableBody = ({ className = '', ...props }: React.HTMLAttributes<HTMLTableSectionElement>) => (
-  <tbody className={`${className}`} {...props} />
-);
-
-export const TableRow = ({ className = '', ...props }: React.HTMLAttributes<HTMLTableRowElement>) => (
-  <tr
-    className={`border-b transition-colors hover:bg-gray-50 ${className}`}
-    {...props}
-  />
-);
-
-export const TableHead = ({ className = '', ...props }: React.ThHTMLAttributes<HTMLTableCellElement>) => (
-  <th
-    className={`h-12 px-4 text-left align-middle font-medium text-gray-500 ${className}`}
-    {...props}
-  />
-);
-
-export const TableCell = ({ className = '', ...props }: React.TdHTMLAttributes<HTMLTableCellElement>) => (
-  <td
-    className={`p-4 align-middle ${className}`}
-    {...props}
-  />
-);
+export default MyTable;
